8d6cf370c926272f6e69e614694eaed9cf519c5a
Quackmen <97483631+Quackmen@users.noreply.github.com>
2022-08-23 11:27:10 -0300

    Commit

--- R/coorte.R
+++ R/coorte.R
@@ -50,61 +50,108 @@ coorte_teste_md <- coorte_teste_md |>
 #is characterized by one or more Manic or Mixed Episodes,
 #usually accompanied by Major Depressive Episodes.
 
-    #Episódio Hipomaníaco
+#Episódio Maníaco Passado
 
 coorte_teste_md <- coorte_teste_md |>
-    mutate(
-        ephip_pa_22 = if_else(
-            (l081 == 1 | l083 == 1) & (l082 == 0 & l084 == 0),
+    mutate(epman_pa_22 = if_else(
+# Se D1a ou D2a e ausência de sintomatologia atual
+        (l081 == 1 | l083 == 1) & (l082 == 0 & l084 == 0),
+        if_else(
+# Se D1a = "Sim"
+            l081 == 1,
             if_else(
+# Se há pelo menos 3 "Sim" em D3 como D1a = "Sim"
                 l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 3,
-                if_else(
-                    l093 == 0 & l094 == 0,
-                    1, 0), if_else(
-                        l081 == 0 & l083 == 1,
-                        if_else(
-                            l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
-                            if_else(
-                                l093 == 0 & l094 == 0,
-                                1, 0), 0
-                            )
-                        )
-                    )
-                )
-            )
-
-coorte_teste_md <- coorte_teste_md |>
-    mutate(
-        ephip_at_22 = if_else(
-            (l081 == 1 | l083 == 1) & (l082 == 1 | l084 == 1),
+# Se D4 é cotada como "Sim"
+                if_else((l092 == 1 & l094 >= 1) | l093 == 1,
+                        1, 0), 0),
             if_else(
-                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 3,
-                if_else(
-                    l093 & l094 == 0,
-                    1, 0), if_else(
-                        l081 == 0 & l083 == 1,
-                        if_else(
-                            l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
-                            if_else(
-                                l093 & l094 == 0,
-                                1, 0), 0
-                        )
-                    )
-            )
-        )
-    )
+# Se há pelo menos 4 "Sim" em D3 como D1a = "Não"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
+# Se D4 é cotada como "Sim"
+                if_else((l092 == 1 & l094 >= 1) | l093 == 1,
+                        1, 0), 0)
+        ), 0
+    ))
+
+#Episódio Maníaco Atual
 
 coorte_teste_md <- coorte_teste_md |>
-    mutate(
-        ephip_mg_22 = if_else(
-            (l081 == 1 | l083 == 1) & (l082 == 0 & l084 == 0),
-        )
-    )
+    mutate(epman_at_22 = if_else(
+# Se D1a ou D2a e presença de sintomatologia atual
+        (l081 == 1 | l083 == 1) & (l082 == 1 | l084 == 1),
+        if_else(
+# Se D1a = "Sim"
+            l081 == 1,
+            if_else(
+# Se há pelo menos 3 "Sim" em D3 como D1a = "Sim"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 3,
+# Se D4 é cotada como "Sim"
+                if_else((l092 == 1 & l094 >= 1) | l093 == 1,
+                        1, 0), 0),
+            if_else(
+# Se há pelo menos 4 "Sim" em D3 como D1a = "Não"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
+# Se D4 é cotada como "Sim"
+                if_else((l092 == 1 & l094 >= 1) | l093 == 1,
+                        1, 0), 0)
+        ), 0
+    ))
 
 #Diagnóstico de Transtorno Bipolar II
 
 #is characterized by one or more Major Depressive Episodes
 #accompanied by at least one Hypomanic Episode.
+
+    #Episódio Hipomaníaco Passado
+
+coorte_teste_md <- coorte_teste_md |>
+    mutate(ephip_pa_22 = if_else(
+        (l081 == 1 | l083 == 1) & (l082 == 0 & l084 == 0),
+#Presença de D1a ou D2a e ausência de sintomatologia atual
+        if_else(
+# Se D1a = "Sim"
+            l081 == 1,
+            if_else(
+# Se há pelo menos 3 "Sim" em D3 como D1a = "Sim"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 3,
+# Se D4 é cotada como "Não"
+                if_else((l092 == 0 | l094 >= 0) & l093 == 0,
+                        1, 0), 0 ),
+            if_else(
+# Se há pelo menos 4 "Sim" em D3 como D1a = "Não"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
+# Se D4 é cotada como "Não"
+                if_else((l092 == 0 | l094 >= 0) & l093 == 0,
+                        1, 0), 0)
+        ), 0
+    ))
+
+#Episódio Hipomaníaco Atual
+
+coorte_teste_md <- coorte_teste_md |>
+    mutate(ephip_at_22 = if_else(
+        (l081 == 1 | l083 == 1) & (l082 == 1 | l084 == 1),
+#Presença de D1a ou D2a e presença de sintomatologia atual
+        if_else(
+            l082 == 1, # Testando se D1b = "Sim"
+            if_else(
+# Se há pelo menos 3 "Sim" em D3 como D1a = "Sim"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 3,
+# Se D4 é cotada como "Não"
+                if_else((l092 == 0 | l094 >= 0) & l093 == 0,
+                        1, 0), 0),
+            if_else(
+# Se há pelo menos 4 "Sim" em D3 como D1a = "Não"
+                l085 + l086 + l087 + l088 + l089 + l090 + l091 >= 4,
+# Se D4 é cotada como "Sim"
+                if_else((l092 == 0 | l094 >= 0) & l093 == 0,
+                        0, 1), 0)
+            ), 0
+    ))
+
+
+
 
 
 
