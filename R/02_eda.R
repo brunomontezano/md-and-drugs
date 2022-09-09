051ee67462750ae2f7407711edf6bb13bd73ce8f
Bruno Montezano <brunodoyt@gmail.com>
2022-09-09 13:27:56 -0300

    add some first impressions on data

--- R/02_eda.R
+++ R/02_eda.R
@@ -3,6 +3,23 @@
 # Ler dados a partir do arquivo exportado no script '01_data.R'
 dados_limpos <- readr::read_rds("data/clean_data.rds")
 
+##### DESCREVER DESFECHO E EXPOSIÇÃO #####
+
 # Dar uma olhada na estrutura
 dados_limpos |>
     dplyr::glimpse()
+
+# Checar incidência de transtorno bipolar aos 22 anos
+dados_limpos |>
+    dplyr::count(bd_any)
+
+# Checar incidência de transtorno bipolar agrupada pela variável de uso
+# de maconha
+dados_limpos |>
+    dplyr::filter(!is.na(maconha)) |>
+    dplyr::group_by(maconha) |>
+    dplyr::count(bd_any)
+
+
+##### DATA VIS ######
+
